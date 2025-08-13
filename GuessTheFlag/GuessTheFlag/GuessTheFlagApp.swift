3463f72060533f81eece3739b7722d1064c9b741
Amelia <amelia@c-riddell.com>
2025-08-13 15:16:17 -0500

    added new proj

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Amelia Riddell on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
