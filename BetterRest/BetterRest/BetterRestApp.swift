3dea3b1842914a2c53590ed0e40a1801c3f8aff6
Amelia <amelia@c-riddell.com>
2025-08-21 15:57:41 -0500

    updated betterrest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Amelia Riddell on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
